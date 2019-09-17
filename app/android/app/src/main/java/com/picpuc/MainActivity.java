431a2fb871a14e4144c69182f052998470cd8cf5
Sir Ortiz <sirortiz@Sirs-iMac.local>
2019-09-17 10:35:30 -0500

    fix app crash android

--- app/android/app/src/main/java/com/picpuc/MainActivity.java
+++ app/android/app/src/main/java/com/picpuc/MainActivity.java
@@ -9,19 +9,13 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "picpuc";
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
-    }
-    @Override
+     @Override
     public void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         setIntent(intent);
+    }
+    @Override
+    protected String getMainComponentName() {
+        return "picpuc";
     }
 }
--- app/android/app/src/main/java/com/picpuc/MainApplication.java
+++ app/android/app/src/main/java/com/picpuc/MainApplication.java
@@ -1,6 +1,7 @@
 package com.picpuc;
 
 import android.app.Application;
+
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.reactnative.ivpusic.imagepicker.PickerPackage;
@@ -9,26 +10,25 @@ import io.invertase.firebase.RNFirebasePackage;
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
-import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.CallbackManager;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
 import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
-import java.util.Arrays;
-import java.util.List;
-import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
+import com.airbnb.android.react.maps.MapsPackage;
 import cl.json.RNSharePackage;
 import cl.json.ShareApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainApplication extends Application implements ReactApplication {
   private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
-
   protected static CallbackManager getCallbackManager() {
     return mCallbackManager;
   }
-   
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -45,11 +45,10 @@ public class MainApplication extends Application implements ReactApplication {
           new VectorIconsPackage(),
           new RNFirebasePackage(),
           new RNGoogleSigninPackage(),
-          new FBSDKPackage(mCallbackManager),
-          new RNFirebaseMessagingPackage(),
-          new RNAndroidLocationEnablerPackage(),
           new MapsPackage(),
-          new RNSharePackage()
+          new FBSDKPackage(mCallbackManager),
+          new RNSharePackage(),
+          new RNFirebaseMessagingPackage()
       );
     }
 
