1230a4bfca49c8b94223d78ddcd4743951ef77ea
Ing. ABV <alexanderbarajas@gmail.com>
2022-06-21 21:15:32 -0500

    corregir issues

--- rds/2022_06_20_create_table_eventos.sql
+++ rds/2022_06_20_create_table_eventos.sql
@@ -1,15 +1,15 @@
-CREATE TABLE IF NOT EXISTS `eventos` (
-  `ideventos` INT NOT NULL,
-  `tittle` VARCHAR(45) NOT NULL,
-  `description` LONGTEXT NOT NULL,
-  `eventDate` DATETIME NOT NULL,
-  `created` VARCHAR(45) NOT NULL,
-  `image` VARCHAR(255) NULL,
-  `idCategory` INT NOT NULL,
-  `namePlace` VARCHAR(45) NOT NULL,
-  `estado` BINARY(1) NOT NULL,
-  PRIMARY KEY (`ideventos`))
-ENGINE = InnoDB;
+CREATE TABLE IF NOT EXISTS eventos (
+  idEventos INT NOT NULL,
+  title VARCHAR(45) NOT NULL,
+  description character VARYING NOT NULL,
+  eventDate timestamp NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW(),
+  image VARCHAR(255) NULL,
+  idCategory INT NOT NULL,
+  namePlace VARCHAR(45) NOT NULL,
+  estado BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (idEventos));
+
 
 COMMENT ON TABLE eventos IS 'events directory';
 
